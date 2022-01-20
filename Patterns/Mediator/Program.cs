1be7b92ef712bd463a4cbc8728c065fae8f59cc9
valeryborutenko <valery.borutenko@p97.com>
2022-01-20 20:03:29 +0300

    mediator + observer pattern

---
+++ Patterns/Mediator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mediator = new ConcreteMediator();
+            var checkbox = new Checkbox(mediator);
+            var textBox = new TextBox(mediator);
+            mediator.TextBox = textBox;
+            mediator.HideCheckbox = checkbox;
+
+            checkbox.Check();
+
+            Console.ReadKey();
+
+        }
+    }
+
+
+    public abstract class Mediator
+    {
+        public abstract void Notify(Component sender, string @event);
+    }
+
+    public class ConcreteMediator : Mediator
+    {
+        public Checkbox HideCheckbox { get; set; }
+
+        public TextBox TextBox { get; set; }
+
+
+        public override void Notify(Component sender, string @event)
+        {
+            if (sender == HideCheckbox && @event == "check")
+            {
+                TextBox.IsVisible = !TextBox.IsVisible;
+            }
+        }
+    }
+
+
+
+    public abstract class Component
+    {
+        protected readonly Mediator _mediator;
+        public bool IsVisible { get; set; }
+
+        protected Component(Mediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+
+    public class TextBox : Component
+    {
+        public TextBox(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public void Click()
+        {
+            _mediator.Notify(this,"click");
+        }
+    }
+
+    public class Checkbox : Component
+    {
+        public Checkbox(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public void Check()
+        {
+            Console.WriteLine("Checkbox was clicked.This info should be delivered to textbox through mediator");
+            _mediator.Notify(this,"check");
+        }
+    }
+}
---
+++ Patterns/Observer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var concreteObservable = new ConcreteObservable();
+            concreteObservable.AddObserver(new ConcreteObserver1());
+            concreteObservable.AddObserver(new ConcreteObserver2());
+            concreteObservable.NotifyObservers();
+            Console.ReadKey();
+        }
+    }
+    interface IObservable
+    {
+        void AddObserver(IObserver o);
+        void RemoveObserver(IObserver o);
+        void NotifyObservers();
+    }
+    class ConcreteObservable : IObservable
+    {
+        private List<IObserver> observers;
+        public ConcreteObservable()
+        {
+            observers = new List<IObserver>();
+        }
+        public void AddObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver observer in observers)
+                observer.Update();
+        }
+    }
+
+    interface IObserver
+    {
+        void Update();
+    }
+    class ConcreteObserver1 : IObserver
+    {
+        public void Update()
+        {
+            Console.WriteLine("The first observer got update");
+        }
+    }
+    class ConcreteObserver2 : IObserver
+    {
+        public void Update()
+        {
+            Console.WriteLine("The second observer got update");
+        }
+    }
+}
