4c19921ab2a64deb33bf976167a20f5ffa6f56fd
valeryborutenko <valery.borutenko@p97.com>
2022-01-20 15:01:26 +0300

    add strategy pattern

---
+++ Patterns/Strategy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var smsSender = new SmsSender();
+            var context = new Context(smsSender);
+            context.Do();
+            Console.ReadKey();
+        }
+    }
+
+
+    public class Context
+    {
+        private readonly ISender _sender;
+        public Context(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        public void Do()
+        {
+            //doing some work
+            
+            _sender.Send();
+        }
+    }
+
+
+    public interface ISender
+    {
+        void Send();
+    }
+
+    public class EmailSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Send to email");
+        }
+    }
+
+    public class SmsSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Send to sms");
+        }
+    }
+}
