74ca285944bdf5cc2c6d8a62122e835e44d3f204
valeryborutenko <valery.borutenko@p97.com>
2022-01-23 14:25:41 +0300

    add memento

---
+++ Patterns/Command/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create invoker
+            var transactionManager = new TransactionManager();
+
+            //create receiver
+            var account = new Account("Ivan", 100);
+
+            var depositCommand = new Deposit(account, 100);
+
+            transactionManager.AddTransaction(depositCommand);
+
+            transactionManager.ProcessPendingTransactions();
+
+        }
+    }
+
+
+    public class Account // receiver
+    {
+        public string OwnerName { get; set; }
+        public decimal Balance { get; set; }
+ 
+        public Account(string ownerName, decimal balance)
+        {
+            OwnerName = ownerName;
+            Balance = balance;
+        }
+    }
+
+
+    public interface ITransaction // command
+    {
+        bool IsCompleted { get; set; }
+        void Execute();
+    }
+
+    public class Deposit : ITransaction // concrete command
+    {
+        private readonly Account _account;
+        private readonly decimal _amount;
+ 
+        public bool IsCompleted { get; set; }
+ 
+        public Deposit(Account account, decimal amount)
+        {
+            _account = account;
+            _amount = amount;
+ 
+            IsCompleted = false;
+        }
+ 
+        public void Execute()
+        {
+            _account.Balance += _amount;
+ 
+            IsCompleted = true;
+        }
+    }
+
+    public class TransactionManager // invoker
+    {
+        private readonly List<ITransaction> _transactions = new List<ITransaction>();
+ 
+        public bool HasPendingTransactions
+        {
+            get { return _transactions.Any(x => !x.IsCompleted); }
+        }
+ 
+        public void AddTransaction(ITransaction transaction)
+        {
+            _transactions.Add(transaction);
+        }
+ 
+        public void ProcessPendingTransactions()
+        {
+            // Apply transactions in the order they were added.
+            foreach(ITransaction transaction in _transactions.Where(x => !x.IsCompleted))
+            {
+                transaction.Execute();
+            }
+        }
+    }
+}
---
+++ Patterns/Memento/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace Memento
+{
+    /// <summary>
+    /// Memento is a behavioral design pattern that lets you save and restore the previous state of an object without revealing the details of its implementation.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // The Originator holds some important state that may change over time. It
+    // also defines a method for saving the state inside a memento and another
+    // method for restoring the state from it.
+    class Originator
+    {
+        private string _state;
+
+        public Originator(string state)
+        {
+            this._state = state;
+            Console.WriteLine("Originator: My initial state is: " + state);
+        }
+
+        // The Originator's business logic may affect its internal state.
+        // Therefore, the client should backup the state before launching
+        // methods of the business logic via the save() method.
+        public void DoSomething()
+        {
+            Console.WriteLine("Originator: I'm doing something important.");
+            _state = this.GenerateRandomString();
+            Console.WriteLine($"Originator: and my state has changed to: {_state}");
+        }
+
+        private string GenerateRandomString()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        // Saves the current state inside a memento.
+        public IMemento Save()
+        {
+            return new ConcreteMemento(this._state);
+        }
+
+        // Restores the Originator's state from a memento object.
+        public void Restore(IMemento memento)
+        {
+            if (!(memento is ConcreteMemento))
+            {
+                throw new Exception("Unknown memento class " + memento.ToString());
+            }
+
+            this._state = memento.GetState();
+            Console.Write($"Originator: My state has changed to: {_state}");
+        }
+    }
+
+    // The Memento interface provides a way to retrieve the memento's metadata,
+    // such as creation date or name. However, it doesn't expose the
+    // Originator's state.
+    public interface IMemento
+    {
+        string GetState();
+    }
+
+    // The Concrete Memento contains the infrastructure for storing the
+    // Originator's state.
+    class ConcreteMemento : IMemento
+    {
+        private string _state;
+
+        public ConcreteMemento(string state)
+        {
+            this._state = state;
+        }
+
+        // The Originator uses this method when restoring its state.
+        public string GetState()
+        {
+            return this._state;
+        }
+
+        // The rest of the methods are used by the Caretaker to display
+    }
+
+    // The Caretaker doesn't depend on the Concrete Memento class. Therefore, it
+    // doesn't have access to the originator's state, stored inside the memento.
+    // It works with all mementos via the base Memento interface.
+    class Caretaker
+    {
+        private List<IMemento> _mementos = new List<IMemento>();
+
+        private Originator _originator = null;
+
+        public Caretaker(Originator originator)
+        {
+            this._originator = originator;
+        }
+
+        public void Backup()
+        {
+            Console.WriteLine("\nCaretaker: Saving Originator's state...");
+            this._mementos.Add(_originator.Save());
+        }
+
+        public void Undo()
+        {
+            if (this._mementos.Count == 0)
+            {
+                return;
+            }
+
+            var memento = this._mementos.Last();
+            this._mementos.Remove(memento);
+
+            try
+            {
+                this._originator.Restore(memento);
+            }
+            catch (Exception)
+            {
+                this.Undo();
+            }
+        }
+
+        public void ShowHistory()
+        {
+            Console.WriteLine("Caretaker: Here's the list of mementos:");
+
+            foreach (var memento in this._mementos)
+            {
+                Console.WriteLine(memento.GetName());
+            }
+        }
+    }
+}
