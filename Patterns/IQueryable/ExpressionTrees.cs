3ea9e2475ad44ecdd919b4b8da649d0e12fbbf0b
valeryborutenko <valery.borutenko@p97.com>
2022-02-06 16:57:13 +0300

    expression trees

---
+++ Patterns/IQueryable/ExpressionTrees.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IQueryable
+{
+    public class ExpressionTrees
+    {
+        /// <summary>
+        /// (х) => Console.WriteLine(х + 5)
+        /// </summary>
+        public static void Test()
+        {
+            ParameterExpression parameter = Expression.Parameter(typeof(double));
+            ConstantExpression constant = Expression.Constant(5d, typeof(double));
+            BinaryExpression add = Expression.Add(parameter, constant);
+            MethodInfo writeLine = typeof(Console).GetMethod(nameof(Console.WriteLine), new[] { typeof(double) });
+            MethodCallExpression methodCall = Expression.Call(null, writeLine, add);
+            Expression<Action<double>> expressionlambda = Expression.Lambda<Action<double>>(methodCall, parameter);
+            Action<double> delegateLambda = expressionlambda.Compile();
+            delegateLambda(123321);
+
+            Expression<Action<double>> write = х => Console.WriteLine(х + 5);
+        }
+
+        
+    }
+}
--- Patterns/IQueryable/Program.cs
+++ Patterns/IQueryable/Program.cs
@@ -14,6 +14,7 @@ namespace IQueryable
     {
         static void Main(string[] args)
         {
+            ExpressionTrees.Test();
         }
     }
 }
