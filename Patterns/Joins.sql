5312349926ef6bb063d05b352fceab8f8acbab81
valeryborutenko <valery.borutenko@p97.com>
2022-02-07 19:28:48 +0300

    joins

--- Patterns/Joins.sql
+++ Patterns/Joins.sql
@@ -1,1 +1,38 @@
-﻿
+﻿-- INNER JOIN
+
+  SELECT c.* , p.LastName
+  FROM [Sales].[Customer] c
+  INNER JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+
+  -- LEFT (OUTER) JOIN
+
+  SELECT c.* , p.LastName
+  FROM [Sales].[Customer] c
+  LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+
+
+  --RIGHT (OUTER) JOIN
+  
+  SELECT c.* , p.LastName
+  FROM [Sales].[Customer] c
+  RIGHT OUTER JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+
+  --FULL (OUTER) JOIN
+
+  SELECT c.* , p.LastName
+  FROM [Sales].[Customer] c
+  FULL JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+
+
+  -- CROSS JOIN
+
+
+  --The CARTESIAN JOIN is also known as CROSS JOIN. In a CARTESIAN JOIN there is a 
+  --join for each row of one table to every row of another table. This usually happens when the matching column or WHERE condition is not specified.
+
+  SELECT c.* , p.LastName
+  FROM [Sales].[Customer] c
+  CROSS JOIN Person.Person p 
+
+
+
